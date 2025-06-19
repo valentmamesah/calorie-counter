5f036e6f8b9dd0a1b1ff39f9dfafd73b7f715776
Valentino Mamesah <152392923+valentmamesah@users.noreply.github.com>
2025-06-19 07:18:51 +0700

    Update calorie_estimator.py

--- food_calorie_estimator/calorie_estimator.py
+++ food_calorie_estimator/calorie_estimator.py
@@ -1,9 +1,11 @@
-
 def hitung_total_kalori(detected_items, kalori_dict):
     rincian = {}
     total_kalori = 0
+
     for item in detected_items:
-        kalori = kalori_dict.get(item, 0)
-        rincian[item] = rincian.get(item, 0) + kalori
+        class_name = item.get("class")  
+        kalori = kalori_dict.get(class_name, 0)
+        rincian[class_name] = rincian.get(class_name, 0) + kalori
         total_kalori += kalori
+
     return total_kalori, rincian
