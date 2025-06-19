c935eeed7532f9f22b935b6b92d1c3b9676e6bb9
Valentino Mamesah <152392923+valentmamesah@users.noreply.github.com>
2025-06-19 07:19:17 +0700

    Update detector.py

--- food_calorie_estimator/detector.py
+++ food_calorie_estimator/detector.py
@@ -1,13 +1,23 @@
-
 from ultralytics import YOLO
 
 class FoodDetector:
     def __init__(self, model_path="food_calorie_estimator/model/yolo_food.pt"):
         self.model = YOLO(model_path)
 
-    def detect_food(self, image_path):
-        results = self.model.predict(image_path, conf=0.3)
-        classes = []
+    def detect_food(self, image_path, conf=0.3):
+        results = self.model.predict(image_path, conf=conf)
+        detections = []
+
         for r in results:
-            classes.extend([r.names[int(cls)] for cls in r.boxes.cls])
-        return classes
+            for box in r.boxes:
+                cls_id = int(box.cls[0])
+                conf_score = float(box.conf[0])
+                xyxy = box.xyxy[0].tolist()  # bbny
+                class_name = r.names[cls_id]
+                detections.append({
+                    "class": class_name,
+                    "bbox": xyxy,
+                    "confidence": conf_score
+                })
+
+        return detections
